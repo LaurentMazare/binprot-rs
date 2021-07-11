f03fe178a1774722584a6fd1bfa0ee91686d3213
laurent <laurent.mazare@gmail.com>
2021-07-12 05:58:37 +0800

    Improve the sexp formatting.

--- examples/rpc_client.rs
+++ examples/rpc_client.rs
@@ -22,7 +22,18 @@ enum Sexp {
 impl std::fmt::Debug for Sexp {
     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Sexp::Atom(atom) => fmt.write_str(&atom),
+            Sexp::Atom(atom) => {
+                if atom.contains(|c: char| !c.is_alphanumeric()) {
+                    fmt.write_str("\"")?;
+                    for c in atom.escape_default() {
+                        std::fmt::Write::write_char(fmt, c)?;
+                    }
+                    fmt.write_str("\"")?;
+                } else {
+                    fmt.write_str(&atom)?;
+                }
+                Ok(())
+            }
             Sexp::List(list) => {
                 fmt.write_str("(")?;
                 for (index, sexp) in list.iter().enumerate() {
