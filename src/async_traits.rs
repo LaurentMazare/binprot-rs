d89be49e10688a411ba444572cc05ec6253af687
laurent <laurent.mazare@gmail.com>
2021-07-19 21:23:44 +0800

    Tweak the function names for the async trait.

--- src/async_traits.rs
+++ src/async_traits.rs
@@ -3,12 +3,14 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[async_trait]
 pub trait BinProtWriteAsync {
-    async fn binprot_write<W: AsyncWriteExt>(&self, w: &mut W) -> std::io::Result<()>;
+    async fn binprot_write_async<W: AsyncWriteExt>(&self, w: &mut W) -> std::io::Result<()>;
 }
 
 #[async_trait]
 pub trait BinProtReadAsync {
-    async fn binprot_read<R: AsyncReadExt + ?Sized>(r: &mut R) -> Result<Self, crate::error::Error>
+    async fn binprot_read_async<R: AsyncReadExt + ?Sized>(
+        r: &mut R,
+    ) -> Result<Self, crate::error::Error>
     where
         Self: Sized;
 }
