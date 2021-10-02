7afaccaae63dab67e98823b03a7488bba7064a83
laurent <laurent.mazare@gmail.com>
2021-10-02 14:12:33 +0100

    Tweak the test.

--- tests/async_tests.rs
+++ tests/async_tests.rs
@@ -23,10 +23,11 @@ struct Breakfasts {
 async fn roundtrip<
     T: 'static + Clone + BinProtRead + BinProtWrite + PartialEq + Debug + Send + Sync,
 >(
-    vs: Vec<T>,
+    vs: &[T],
 ) -> Result<(), binprot::Error> {
     let (mut client, mut server) = tokio::io::duplex(1);
-    let vs_for_spawn = vs.clone();
+    let mut vs_for_spawn = vec![];
+    vs_for_spawn.extend_from_slice(vs);
     tokio::spawn(async move {
         let mut buffer = binprot::AsyncBuffer::new(1);
         for v in vs_for_spawn.iter() {
@@ -44,14 +45,14 @@ async fn roundtrip<
 #[cfg(feature = "async")]
 #[tokio::test]
 async fn roundtrip_test() -> Result<(), binprot::Error> {
-    roundtrip(vec![Pancakes(42); 100]).await?;
-    roundtrip(vec![Pancakes(42), Pancakes(43), Pancakes(44)]).await?;
+    roundtrip(&vec![Pancakes(42); 100]).await?;
+    roundtrip(&vec![Pancakes(42), Pancakes(43), Pancakes(44)]).await?;
     let breakfasts = Breakfasts {
         pancakes: Pancakes(12),
         more_pancakes: MorePancakes(-123, 2.71828182846, 0),
         value1: -1234567890123456,
         value2: (3.141592, 6535.8979),
     };
-    roundtrip(vec![breakfasts; 100]).await?;
+    roundtrip(&vec![breakfasts; 100]).await?;
     Ok(())
 }
