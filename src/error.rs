4253b95d4a36ab8abbc4ee930fc06755a2048340
laurent <laurent.mazare@gmail.com>
2021-11-19 22:13:12 +0000

    Support more error types.

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,22 @@
 #[derive(Debug)]
 pub enum Error {
     IoError(std::io::Error),
-    UnexpectedVariantIndex { index: u8, ident: &'static str },
-    UnexpectedPolymorphicVariantIndex { index: i32, ident: &'static str },
+    UnexpectedVariantIndex {
+        index: u8,
+        ident: &'static str,
+    },
+    UnexpectedPolymorphicVariantIndex {
+        index: i32,
+        ident: &'static str,
+    },
     UnexpectedValueForUnit(u8),
     UnexpectedValueForBool(u8),
     UnexpectedValueForOption(u8),
     Utf8Error(std::str::Utf8Error),
     SameKeyAppearsTwiceInMap,
+    TryFromIntError(std::num::TryFromIntError),
+    /// For errors raised by custom decoders.
+    CustomError(Box<dyn std::error::Error + Sync + Send>),
 }
 
 impl std::fmt::Display for Error {
@@ -21,6 +30,12 @@ impl std::error::Error for Error {}
 impl From<std::io::Error> for Error {
     fn from(e: std::io::Error) -> Self {
         Error::IoError(e)
+    }
+}
+
+impl From<std::num::TryFromIntError> for Error {
+    fn from(e: std::num::TryFromIntError) -> Self {
+        Error::TryFromIntError(e)
     }
 }
 
